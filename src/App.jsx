217f020e8526ea016920488fbed97ba56128bcf9
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2021-11-03 16:34:41 +0900

    I started a project using react and bootstrap based on a tutorial.  I am just following the tutorial.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+export class NavBar extends Component{
+    render(){
+        return (
+        <React.Fragment>
+            <h1>Hello World!</h1>
+            <p class="highlight">paragraph</p>
+            <button class="btn btn-primary">Hello</button>
+        </React.Fragment>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "jquery";
+import "@popperjs/core";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import {NavBar} from "./App";
+import "./index.css";
+
+ReactDOM.render(<NavBar />, document.getElementById("root"))
+
